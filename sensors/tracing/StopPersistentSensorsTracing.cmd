f2b8c75f2466eb462ba37da6fd27ccf3ced1444c
Jocelyn Berrendonner [MSFT] <Jocelynb@microsoft.com>
2018-01-19 18:22:45 -0800

    Updating the script to support non-desktop systems
    Updating the script to support non-desktop systems

--- sensors/tracing/StopPersistentSensorsTracing.cmd
+++ sensors/tracing/StopPersistentSensorsTracing.cmd
@@ -2,6 +2,9 @@
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
+IF EXIST c:\windows\system32\logman.exe (
+echo Running logging for desktop systems...
+
 CALL WHOAMI.EXE /GROUPS | FIND.EXE /I "S-1-16-12288" >nul
 IF ERRORLEVEL 1 (
     ECHO This script must be run from an elevated prompt.
@@ -22,3 +25,19 @@ copy %ProgramData%\Microsoft\WDF\WudfTrace.etl %SystemRoot%\Tracing >nul 2>&1
 copy %ProgramData%\Microsoft\WDF\*.dmp %SystemRoot%\Tracing >nul 2>&1
 
 start %SystemRoot%\Tracing
+
+) ELSE (
+echo Running logging for other systems...
+
+echo Stopping trace...
+tracelog -stop SensorsTrace >nul
+tracelog -remove SensorsTrace >nul
+echo Now collecting version info
+reg query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v BuildLabEX >> %SystemRoot%\Tracing\BuildNumber.txt
+dir /s %SystemRoot%\LiveKernelReports\* >> %SystemRoot%\Tracing\BuildNumber.txt
+
+rem copying the WUDF traces
+copy %ProgramData%\Microsoft\WDF\WudfTrace.etl %SystemRoot%\Tracing >nul 2>&1
+copy %ProgramData%\Microsoft\WDF\*.dmp %SystemRoot%\Tracing >nul 2>&1
+
+)

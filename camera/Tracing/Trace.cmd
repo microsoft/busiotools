d94c225e468b72d7121f2e224002c64f51eceb1e
Jenny Leung <jeleung@microsoft.com>
2020-02-12 15:52:06 -0800

    fix trace.cmd to use pwsh.exe when powershell.exe doesn't exist

--- camera/Tracing/Trace.cmd
+++ camera/Tracing/Trace.cmd
@@ -5,14 +5,25 @@ REM Redirect /?, /h, etc. to help.
 for %%a in (./ .- .) do if ".%~1." == "%%a?." goto Usage
 for %%a in (./ .- .) do if ".%~1." == "%%ah." goto Usage
 
+powershell.exe /?  > nul 2>&1
+ if ERRORLEVEL 9009 ( 
+    SET ps=pwsh.exe
+) ELSE ( 
+    SET ps=powershell.exe
+)
+
 if "%1" == "elevated" (
-    REM Remove Zone.Identifier in case the script was marked as downloaded from the Internet
-    powershell -Command "Get-ChildItem %~dp0 -Include *.ps1,*.psm1 -Recurse | Unblock-File" > nul
+    REM Remove Zone.Identifier in case the script was marked as downloaded from the Internet.
 
-    powershell -ExecutionPolicy Bypass -File "%~dp0Trace.ps1" 
+    %ps% -Command "Get-ChildItem %~dp0 -Include *.ps1,*.psm1 -Recurse | Unblock-File" > nul
 
+    if "%2" == "" (
+        %ps% -ExecutionPolicy Bypass -File "%~dp0Trace.ps1" %3 %4 %5 %6 %7 %8 %9
+    ) else (
+        %ps% -ExecutionPolicy Bypass -File "%~dp0Trace.ps1" -Scenario %2 %3 %4 %5 %6 %7 %8 %9
+    )
 ) else (
-    powershell -Command "Start-Process %0 -Verb Runas -ArgumentList 'elevated %1 %2 %3 %4 %5 %6 %7 %8'"
+    %ps% -Command "Start-Process %0 -Verb Runas -ArgumentList 'elevated %1 %2 %3 %4 %5 %6 %7 %8'"
 )
 
 goto :EOF
@@ -22,8 +33,10 @@ echo.
 echo Capture ETW traces.
 echo.
 echo A variety of ETW provider types are supported:
-echo TraceLogging, EventSource, WPP, one-line ETW.
+echo Manifest-based ETW, TraceLogging, EventSource, WPP, one-line ETW.
 echo.
 echo USAGE:
-echo     Trace             Capture trace for multimedia scenario
+echo     Trace             Capture using the default scenario.
+echo     Trace scenario    Capture using a particular scenario.
 echo.
+echo Scenarios control which set of ETW event providers get enabled.

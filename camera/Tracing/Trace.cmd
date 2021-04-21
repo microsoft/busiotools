5cc6b26a0059c39e9edf5e43a374253792b5bd77
Jenny Leung <jeleung@microsoft.com>
2021-04-21 00:39:39 -0700

    fix Trace.cmd, update readme, add script log

--- camera/Tracing/Trace.cmd
+++ camera/Tracing/Trace.cmd
@@ -5,10 +5,10 @@ REM Redirect /?, /h, etc. to help.
 for %%a in (./ .- .) do if ".%~1." == "%%a?." goto Usage
 for %%a in (./ .- .) do if ".%~1." == "%%ah." goto Usage
 
-powershell.exe /?  > nul 2>&1
- if ERRORLEVEL 9009 ( 
+powershell.exe /? >nul 2>&1
+if %ERRORLEVEL% NEQ 0 ( 
     SET ps=pwsh.exe
-) ELSE ( 
+) else ( 
     SET ps=powershell.exe
 )
 
@@ -21,7 +21,7 @@ if "%1" == "elevated" (
 ) else (
     %ps% -Command "Start-Process %0 -Verb Runas -ArgumentList 'elevated %1 %2 %3 %4 %5 %6 %7 %8'"
     REM In FactoryOS, launching newshell using start-process is not supported, just try to run the script directly.
-    if ERRORLEVEL NEQ 0 (
+    if %ERRORLEVEL% NEQ 0 ( 
         %ps% -Command "Get-ChildItem %~dp0 -Include *.ps1,*.psm1 -Recurse | Unblock-File" > nul
         %ps% -ExecutionPolicy Bypass -File "%~dp0Trace.ps1" 
     )
